e06ddb61d4d1f914e527aef344573bb13ec7e76a
TreecYSong <zys398526947@qq.com>
2015-09-30 10:08:48 +0800

    init

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function(request,response){
+	console.log('%s','New Request');
+	response.writeHead(200,{'Content-Type':'text/plain'});
+	response.write('Hello Anna!');
+	response.end();
+}).listen(8888)
